7847cbc2e41cca1cac292ab7b883721c5687a73d
AnusreeMP <anuspreeprakash730@gmail.com>
2026-02-11 12:46:16 +0530

    Added PostgreSQL config

--- edumentor/edumentor/settings.py
+++ edumentor/edumentor/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 from datetime import timedelta
 
@@ -88,11 +89,11 @@ WSGI_APPLICATION = 'edumentor.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
+
 
 
 # Password validation
