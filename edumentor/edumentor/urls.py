bc02e2932a5f96012a17cb22279afe83e738b071
AnusreeMP <anuspreeprakash730@gmail.com>
2026-02-11 14:04:47 +0530

    last

--- edumentor/edumentor/urls.py
+++ edumentor/edumentor/urls.py
@@ -16,11 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse("EduMentor Backend is Live 🚀")
 
 
 urlpatterns = [
+    path('', home), 
     path('admin/', admin.site.urls),
     path('api/', include('app.urls')),
-
      
 ]
